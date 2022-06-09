35eb9088f672961bb465428b2b3cbcdd049dc3a3
niraj-inexture <niraj.inexture@gmail.com>
2022-06-10 02:01:29 +0530

    initial commit

--- pinterest/settings.py
+++ pinterest/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 # STATICFILES_DIRS = [STATIC_DIR]
-STATIC_ROOT = BASE_DIR/'static'
+STATIC_ROOT = BASE_DIR/'staticfiles'
 
 MEDIA_ROOT = BASE_DIR/'media'
 MEDIA_URL = '/media/'
