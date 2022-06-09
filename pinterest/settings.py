f45ea9a60936965a6195df84ba5ccf0e59222fd7
niraj-inexture <niraj.inexture@gmail.com>
2022-06-10 01:13:15 +0530

    something changes in setup

--- pinterest/settings.py
+++ pinterest/settings.py
@@ -131,7 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [STATIC_DIR]
+# STATICFILES_DIRS = [STATIC_DIR]
+STATIC_ROOT = BASE_DIR/'static'
+
 MEDIA_ROOT = BASE_DIR/'media'
 MEDIA_URL = '/media/'
 
