603092e7bddec3eb9f746ca1456820165fee0fd1
niraj-inexture <niraj.inexture@gmail.com>
2022-06-10 11:21:58 +0530

    update type od css

--- registration/urls.py
+++ registration/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('register/', views.RegisterClassView.as_view(),name='register'),
     path('login/',views.LoginClassView.as_view(),name='login'),
     path('logout/',views.LogoutClassView.as_view(),name='logout')
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
