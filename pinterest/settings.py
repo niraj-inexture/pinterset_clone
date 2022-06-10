51aed408cc9b9cf30bf428872b9776321a15536a
niraj-inexture <niraj.inexture@gmail.com>
2022-06-10 10:43:59 +0530

    update setting

--- pinterest/settings.py
+++ pinterest/settings.py
@@ -17,7 +17,6 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_DIR = BASE_DIR/'static'
 TEMPLATES_DIR = BASE_DIR/'template'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
--- registration/urls.py
+++ registration/urls.py
@@ -8,3 +8,5 @@ urlpatterns = [
     path('login/',views.LoginClassView.as_view(),name='login'),
     path('logout/',views.LogoutClassView.as_view(),name='logout')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
